b27b07fea6d40d23eaca6a815e46878add1b179e
Rafaelszc <rafaelbjj84@gmail.com>
2025-12-16 11:39:05 -0300

    feat: implementacão do quick sort adicionada

---
+++ src/sortAlgorithms/quick/QuickSort.java
@@ -0,0 +1,35 @@
+package sortAlgorithms.quick;
+
+import sortAlgorithms.Sorting;
+
+public class QuickSort extends Sorting {
+    public static void sort(int[] arr) {
+        sort(arr, 0, arr.length-1);
+    }
+    private static void sort(int[] arr, int left, int right) {
+        if (left < right){
+            int pivotIndex = partition(arr, left, right);
+            sort(arr, left, pivotIndex - 1);
+            sort(arr, pivotIndex + 1, right);
+        }
+    }
+    private static int partition(int[] arr, int left, int right) {
+        int pivot = arr[left];
+        int i = left;
+        int j = right;
+
+        while (i < j) {
+            if (arr[j] >= pivot) {
+                j--;
+            }
+            else if (arr[i] <= pivot) {
+                i++;
+            }
+            else swap(arr, i, j);
+        }
+        swap(arr, left, j);
+
+        return i;
+    }
+
+}
