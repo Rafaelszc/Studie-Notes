ad21c2aff22c89043559e7742495531de4468e5c
Rafaelszc <rafaelbjj84@gmail.com>
2025-12-17 14:58:13 -0300

    add: SelectionSort implementado

--- src/sortAlgorithms/selection/SelectionSort.java
+++ src/sortAlgorithms/selection/SelectionSort.java
@@ -1,4 +1,18 @@
 package sortAlgorithms.selection;
 
-public class SelectionSort {
+import sortAlgorithms.Sorting;
+
+public class SelectionSort extends Sorting {
+    public static void sort(int[] arr) {
+        for (int i=0; i<arr.length-1; i++) {
+            int min = i;
+            for (int j=i+1; j<arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            swap(arr, i, min);
+        }
+        printArray(arr);
+    }
 }
