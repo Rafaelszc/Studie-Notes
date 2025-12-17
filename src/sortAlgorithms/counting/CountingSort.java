d6585dfd1f05631823b78e7f6a6ff2766403174b
Rafaelszc <rafaelbjj84@gmail.com>
2025-12-17 14:55:58 -0300

    feat: CountingSort implementado

--- src/sortAlgorithms/counting/CountingSort.java
+++ src/sortAlgorithms/counting/CountingSort.java
@@ -1,4 +1,36 @@
 package sortAlgorithms.counting;
 
-public class CountingSort {
+import sortAlgorithms.Sorting;
+
+public class CountingSort extends Sorting {
+    public static void sort(int[] arr) {
+        int k = arr[0];
+
+        for (int i=1; i<arr.length; i++) {
+            k = Math.max(k, arr[i]);
+        }
+
+        int[] arrB = new int[arr.length];
+        int[] arrC = new int[k];
+
+        for (int i=0; i<k; i++) {
+            arrC[i] = 0;
+        }
+
+        for (int i=0; i<arr.length; i++) {
+            arrC[arr[i]-1]++;
+        }
+
+        for (int i=1; i<k; i++) {
+            arrC[i] += arrC[i-1];
+        }
+
+        for (int i=arr.length-1; i>=0; i--) {
+            arrB[arrC[arr[i]-1]-1] = arr[i];
+            arrC[arr[i]-1]--;
+        }
+
+        arr = arrB;
+        printArray(arr);
+    }
 }
