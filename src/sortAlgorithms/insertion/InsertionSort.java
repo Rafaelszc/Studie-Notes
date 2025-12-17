94780838f1ff7bd75d0f2943e6f2506093555f06
Rafaelszc <rafaelbjj84@gmail.com>
2025-12-17 14:56:20 -0300

    add: InsertionSort implementado

--- src/sortAlgorithms/insertion/InsertionSort.java
+++ src/sortAlgorithms/insertion/InsertionSort.java
@@ -1,4 +1,19 @@
 package sortAlgorithms.insertion;
 
-public class InsertionSort {
+import sortAlgorithms.Sorting;
+
+public class InsertionSort extends Sorting {
+    public static void sort(int[] arr) {
+        for (int j=0; j<arr.length; j++) {
+            int key = arr[j];
+
+            int i=j;
+            while (i>0 && key < arr[i-1]) {
+                arr[i] = arr[i-1];
+                i--;
+            }
+            arr[i] = key;
+        }
+        printArray(arr);
+    }
 }
